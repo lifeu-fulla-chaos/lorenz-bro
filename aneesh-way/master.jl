56d2bdb13ad22f1c3fd41fc1da439eacd232fe0c
mbuyt0n <shusrith03@gmail.com>
2025-02-17 07:08:18 +0530

    well ill be damned

--- aneesh-way/master.jl
+++ aneesh-way/master.jl
@@ -33,15 +33,43 @@ function run_master()
     while true
         client = accept(server)
         println("Master: Slave connected.")
-
+        global sol_master
         for i in t_eval
             x = sol_master(i)
             write(client, join(x, ",") * "\n")  # Convert to CSV format
             flush(client)
         end
+        write(client, "END\n")
+        flush(client)
 
         println("Master: Finished sending state data to slave.")
+        println("waiting for slave")
+        line = try
+            readline(client)
+        catch
+            ""
+        end
+        if isempty(line)
+            println("Master: Slave disconnected.")
+        end
+        x = try
+            parse.(Float64, split(line, ","))
+        catch
+            println("Master: Error parsing data.")
+            break
+        end
+        println("Master: Received control data from slave.")
+        prob_master = ODEProblem(lorenz_master!, x, (7.0, 100.0))
+        sol_master = solve(prob_master, Tsit5(), dtmax=dt)
+        for i in sol_master.t
+            x = sol_master(i)
+            write(client, join(x, ",") * "\n")  
+            flush(client)
+        end
+        write(client, "END\n")
+        flush(client)
         close(client)
+        println("Master: Finished sending state data to slave.")
         break
     end
 
--- aneesh-way/slave.jl
+++ aneesh-way/slave.jl
@@ -7,19 +7,17 @@ using LinearAlgebra
 
 
 function run_synchronization()
-    # Lorenz system for slave with backstepping control
     function lorenz_slave!(dy, y, p, t)
-        σ, ρ, β, x, u = p  # p contains master state and control input
+        σ, ρ, β, u = p  
         
         dy[1] = σ * (y[2] - y[1]) + u[1]
         dy[2] = y[1] * (ρ - y[3]) - y[2] + u[2]
         dy[3] = y[1] * y[2] - β * y[3] + u[3]
     end
 
-    # Backstepping control law
     function backstepping_control(x, y, p)
         σ, ρ, β = p
-        e = y .- x  # Error vector
+        e = y .- x  
         
         u1 = -σ * (e[2] - e[1]) + e[2]
         u2 = -ρ * e[1] + e[2] + (y[1] * y[3]) - (x[1] * x[3]) + e[3]
@@ -27,10 +25,9 @@ function run_synchronization()
         
         return SVector(u1, u2, u3), e
     end
-    k = 5.0  # Backstepping gain
+    k = 5.0  
     p = (10.0, 28.0, 8 / 3)
-    # Initial conditions
-    y0 = [5.0, 5.0, 5.0]  # Slave initial state (different from master)
+    y0 = [5.0, 5.0, 5.0]  
     dt = 0.001
     T = 7.0
     t_eval = 0:dt:T
@@ -46,15 +43,15 @@ function run_synchronization()
         end
         println("Slave: Connected to master.")
 
-        # Receive master states (not dx/dt) from the master
         master_x = []
         while true
             line = try
                 readline(client)
             catch
                 ""
+            
             end
-            if isempty(line)
+            if isempty(line) || line == "END"
                 break
             end
 
@@ -66,8 +63,7 @@ function run_synchronization()
             end
             push!(master_x, x)
         end
-        close(client)  # Close connection after receiving all data
-
+    
         if isempty(master_x)
             println("Slave: No data received. Exiting.")
             return
@@ -79,14 +75,12 @@ function run_synchronization()
         y_current = copy(y0)
         lowest = Inf
         minarg = -1
-
         for (i, t) in enumerate(t_eval)
             x = master_x[i, :] 
             u, e = backstepping_control(x, y_current, p)
             
-            prob_slave = ODEProblem(lorenz_slave!, y_current, (t, t+dt), (p..., x, u))
+            prob_slave = ODEProblem(lorenz_slave!, y_current, (t, t+dt), (p..., u))
             sol_slave = solve(prob_slave, Tsit5(), dtmax=dt)
-            
             y_current = sol_slave.u[end]
             y_traj[:, i] = y_current
             e_traj[:, i] = e
@@ -95,23 +89,64 @@ function run_synchronization()
                 minarg = i
             end
         end
-        
+
         println("lowest", e_traj[:, minarg], norm(e_traj[:, minarg]))
+        y0 = y_traj[:, minarg]
+        write(client, join(y0, ",") * "\n")
+        flush(client)
+        prob_slave = ODEProblem(lorenz_slave!, y0, (7.0, 100.0), (p..., [0.0, 0.0, 0.0]))
+        sol_slave = solve(prob_slave, Tsit5(), dtmax=dt)
+        y_traj1 = sol_slave.u
+        final_master_x = []
+        while true
+            line = try
+                readline(client)
+            catch
+                ""
+            end
+
+            if isempty(line) || strip(line) == "END"
+                println("Slave: Received final end of transmission.")
+                break
+            end
+
+            x = try
+                parse.(Float64, split(line, ","))
+            catch
+                println("Slave: Error parsing data.")
+                continue
+            end
+            push!(final_master_x, x)
+        end
+        close(client)
+        final_master_x = hcat(final_master_x...)'
+        y_traj1 = hcat(y_traj1...)'
+        println("Slave: Received final master state.")
+        e_traj1 = final_master_x .- y_traj1
         for j in 1:3
-            # State trajectories
             p1 = plot(t_eval, [master_x[:, j] y_traj[j, :]], 
                  label=["Master $("xyz"[j])" "Slave $("xyz"[j])"],
                  xlabel="Time", ylabel="State", 
                  title="Master-Slave Trajectories: Component $("xyz"[j])")
             savefig(p1, "trajectory$("xyz"[j]).png")
             
-            # Error plots
             p2 = plot(t_eval, e_traj[j, :], 
                  label="Error $("xyz"[j])", 
                  xlabel="Time", ylabel="Error",
                  title="Synchronization Error: Component $("xyz"[j])")
             savefig(p2, "error$("xyz"[j]).png")
-            
+
+            p3 = plot(sol_slave.t, [final_master_x[:, j] y_traj1[:, j]], 
+                 label=["Master $("xyz"[j])" "Slave $("xyz"[j])"],
+                 xlabel="Time", ylabel="State", 
+                 title="Master-Slave Trajectories: Component $("xyz"[j])")
+            savefig(p3, "final_trajectory$("xyz"[j]).png")
+
+            p4 = plot(sol_slave.t, e_traj1[:, j], 
+                 label="Error $("xyz"[j])", 
+                 xlabel="Time", ylabel="Error",
+                 title="Synchronization Error: Component $("xyz"[j])")
+            savefig(p4, "final_error$("xyz"[j]).png")
         end
 
         println("All plots have been saved.")
